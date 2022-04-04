a8f5f60e4f0b1cd43157fb4087e8639b5032ed6b
Mirim <Mirim@PC-06>
2022-04-04 12:00:37 +0900

    sizeof(arr[0]) 반복문

--- CodingTest/01 2차원 배열.c
+++ CodingTest/01 2차원 배열.c
@@ -3,8 +3,8 @@
 int main(void)
 {
 	int arr[12] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-	//
-	for (int i = 0; i < sizeof(arr)/sizeof(int); i++) {
+	//sizeof(arr[0])  arr[0] 공간 하나의 크기 타입이 바뀌어도 O
+	for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
 		printf("%d\n",arr[i]);
 	}
 }
