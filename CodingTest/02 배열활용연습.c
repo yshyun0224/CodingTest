46f40788ac9543c4c20b26a874926584e300f6a5
Mirim <Mirim@PC-06>
2022-04-11 12:11:58 +0900

    getMax, 최댓값 구하기 함수화

--- CodingTest/02 배열활용연습.c
+++ CodingTest/02 배열활용연습.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
-int main(void)
-{
-	//최대값은 14(arr[9])
-	int arr[10] = { 8, 5, 1, 6, 4, 9, 10, 14, 7, 11};
+
+//배열 요소중 최대값과 그에 해당하는 idx값을 구하시오
+void getMax(int* arr, int length) {
 	int max = 0;
 	int idxMax = 0;
+
 	
-	//배열 요소중 최대값과 그에 해당하는 idx값을 구하시오
-	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+	for (int i = 0; i < length; i++) {
 		if (max < arr[i]) {
 			max = arr[i];
 			idxMax = i;
 		}
 	}
 	printf("최댓값 : %d, 위치 : %d", max, idxMax);
+}
+
+int main(void)
+{
+	//최대값은 14(arr[9])
+	int arr1[10] = { 8, 5, 1, 6, 4, 9, 10, 14, 7, 11};
+
+	getMax(arr1, 10);
+	//sizeof(arr1)/sizeof(arr1[0])
 
 	return 0;
 }
