b4a22025cde6971743d80d6944f1cda12675109e
Mirim <Mirim@PC-06>
2022-04-18 11:34:39 +0900

    최솟값 구하기 함수화

--- CodingTest/02 배열활용연습.c
+++ CodingTest/02 배열활용연습.c
@@ -16,25 +16,28 @@ void getMax(int* arr, int length) {
 	printf("최댓값 : %d, 위치 : %d", max, idxMax);
 }
 
-int main(void)
-{
-	//최대값은 14(arr[9])
-	int arr1[10] = { 8, 5, 1, 6, 4, 9, 10, 14, 7, 11};
-
-	getMax(arr1, 10);
-	//sizeof(arr1)/sizeof(arr1[0])
-	
-	//함수 쓰지 않고 최소값과 idx 구하기
+void getMin(int* arr, int length) {
 	int min = 101;
 	int idxMin = 0;
 
-	for (int i = 0; i < sizeof(arr1) / sizeof(arr1[0]); i++) {
-		if (min > arr1[i]) {
-			min = arr1[i];
+	for (int i = 0; i < length; i++) {
+		if (min > arr[i]) {
+			min = arr[i];
 			idxMin = i;
 		}
 	}
 	printf("\n최솟값 : %d, 위치 : %d", min, idxMin);
+}
 
+int main(void)
+{
+	//최대값은 14(arr[9])
+	//최솟값은 1(arr[2])
+	int arr1[10] = { 8, 5, 1, 6, 4, 9, 10, 14, 7, 11};
+
+	getMax(arr1, 10);
+	//sizeof(arr1)/sizeof(arr1[0])
+	getMin(arr1, 10);
+	
 	return 0;
 }
