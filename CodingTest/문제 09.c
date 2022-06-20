3a8779d31c5fefa408e6a98ee2a5e23b43c41ace
Mirim <Mirim@PC-06>
2022-06-20 12:20:29 +0900

    배열 동적 할당  : malloc (배열을 return 할 때는 동적 할당)

--- CodingTest/문제 09.c
+++ CodingTest/문제 09.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
 int* solution(char* shirt_size[], int shirt_size_len) {
-	int arr[6] = { 0, 0, 0, 0, 0, 0 };
-	int* answer = arr;
+	// 동적 할당
+	int* answer = malloc(6 * sizeof(int));
+	//배열 초기화
+	for (int i = 0; i < 6; i++) {
+		answer[i] = 0;
+	}
 
 	for (int i = 0; i < shirt_size; i++) {
 		if (strcmp(shirt_size[i], "XS") == 0)
 			answer[0]++;
+		else if (strcmp(shirt_size[i], "S") == 0)
+			answer[1]++;
+		else if (strcmp(shirt_size[i], "M") == 0)
+			answer[2]++;
+		else if (strcmp(shirt_size[i], "L") == 0)
+			answer[3]++;
+		else if (strcmp(shirt_size[i], "XL") == 0)
+			answer[4]++;
+		else if (strcmp(shirt_size[i], "XXL") == 0)
+			answer[5]++;
 	}
 
 	return answer;
