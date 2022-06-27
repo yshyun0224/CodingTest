545127402072defb4494a92ab94d51b3fd0a247e
Mirim <Mirim@PC-06>
2022-06-27 10:56:50 +0900

    문자열 비교하기 strcmp (같으면 0을 반환)

--- CodingTest/1학기 수행평가 대비.c
+++ CodingTest/1학기 수행평가 대비.c
@@ -2,19 +2,25 @@
 #include <stdio.h>
 
 int main(void) {
-	//배열은 20개, 문자길이는 3
+	// 배열은 20개, 문자길이는 3
 	char str[20] = "YSY";
 	
-	//문자의 길이를 구하는 함수
+	// 문자의 길이를 구하는 함수
 	int length = strlen(str);
 	printf("%d \n", length);
 
-	//문자열 복사 strcpy(복사공간, 복사대상)
+	// 문자열 복사 strcpy(복사공간, 복사대상)
 	char copy_str[15];
 	strcpy(copy_str, str);
 	printf("%s \n", copy_str);
 
-	//문자열 덧붙이기 strcat(덧붙일 공간, 덧붙일 문자)
+	// 문자열 덧붙이기 strcat(덧붙일 공간, 덧붙일 문자)
 	strcat(copy_str, "BLY");
 	printf("%s \n", copy_str);
+	
+	// 문자열 비교하기
+	if (strcmp(str, "YSY") == 0)
+		printf("양서영 입니다. \n");
+	else
+		printf("양서영이 아닙니다. \n");
 }
